3af63ad638ab8d56b3ee77a1bf0348a3d64d48bb
affanhmalik <affanhmalik@gmail.com>
2014-12-10 20:22:53 -0800

    Implemented a sample Http service to get data from a mock api. It works!

--- Users/Affan/AndroidStudioProjects/GuildfordMusalla/mobile/src/main/java/com/affanhmalik/guildfordmusalla/MainActivity.java
+++ Users/Affan/AndroidStudioProjects/GuildfordMusalla/mobile/src/main/java/com/affanhmalik/guildfordmusalla/MainActivity.java
@@ -1,6 +1,10 @@
 package com.affanhmalik.guildfordmusalla;
 
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.AsyncTask;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
@@ -8,6 +12,7 @@ import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ProgressBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
@@ -20,20 +25,22 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
 //        loader = (ProgressBar) findViewById(R.id.progressBar);
 
-//        Button click test
 
-//        final Button button = (Button) findViewById(R.id.button1);
 
-/*
+        final Button button = (Button) findViewById(R.id.getData);
+//        final TextView output = (TextView) findViewById();
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 sampleDataServiceImplementation();
             }
         });
-*/
     }
 
 
@@ -59,50 +66,65 @@ public class MainActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
+    protected boolean isOnline(){
+        ConnectivityManager cm = (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo netInfo = cm.getActiveNetworkInfo();
+
+        if (netInfo != null && netInfo.isConnectedOrConnecting()){
+            return true;
+        }else{
+            return false;
+        }
+    }
+
     public boolean sampleDataServiceImplementation(){
         /* Implement the data stuff here and get data through the AsyncTask class*/
+        if (isOnline()){
+            requestAsyncData("http://private-e86d1-novella1.apiary-mock.com/login");
+        }else{
+            Toast.makeText(this, "No Network Connectivity", Toast.LENGTH_LONG);
+        }
 
-        BackgroundREST sample = new BackgroundREST();
 
-        sample.execute("Some params");
-
-        return true;
+        return false;
     }
+
+    private void requestAsyncData(String uri) {
+        MainActivity.BackgroundREST sample = new MainActivity.BackgroundREST();
+
+        sample.executeOnExecutor(AsyncTask.THREAD_POOL_EXECUTOR, uri);
+    }
+
 
     private class BackgroundREST extends AsyncTask<String, String, String> {
 
         @Override
         protected void onPreExecute() {
             /* Maybe do a toast saying "contacting server for data" */
-            loader.setVisibility(View.VISIBLE);
-            Toast.makeText(getApplicationContext(), "Beginning...", Toast.LENGTH_SHORT).show();
+
+            /*loader.setVisibility(View.VISIBLE);
+            Toast.makeText(getApplicationContext(), "Beginning...", Toast.LENGTH_SHORT).show();*/
         }
 
         @Override
         protected String doInBackground(String... params) {
             /* Do whatever and then send the result which is picked up by onPostExecute */
-            String a = "asdf";
 
-            /* Publish progress part */
+            String content = HttpManager.getData(params[0]);
 
-            for (int i=0; i < 10 ; i++) {
-                publishProgress("Yada yada yada" + i);
-
-                try {
-                    Thread.sleep(2000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+            return content;
 
 
-            return a;
         }
 
         @Override
         protected void onPostExecute(String s) {
-            loader.setVisibility(View.GONE);
-            Toast.makeText(getApplicationContext(), "s", Toast.LENGTH_SHORT).show();
+            TextView resultBox = (TextView) findViewById(R.id.dataSample);
+
+            resultBox.setText(s);
+
+            /*loader.setVisibility(View.GONE);
+            Toast.makeText(getApplicationContext(), "s", Toast.LENGTH_SHORT).show();*/
         }
 
         @Override
